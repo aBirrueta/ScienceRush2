3be4cec6ab36b5cc1cb13b0f4211f66cbb1b2b60
aBirrueta <alejandrobirru3ta@gmail.com>
2025-01-08 09:29:44 -0800

    Initial Commit

---
+++ ScienceRush2/ScienceRush2App.swift
@@ -0,0 +1,17 @@
+//
+//  ScienceRush2App.swift
+//  ScienceRush2
+//
+//  Created by Alejandro Birrueta on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ScienceRush2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
