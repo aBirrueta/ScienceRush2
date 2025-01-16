37a73a003bf5f817ea4c33ba92e8b1750275de84
aBirrueta <alejandrobirru3ta@gmail.com>
2025-01-16 10:07:54 -0800

    working on looping background not working yet

--- ScienceRush2/GameView.swift
+++ ScienceRush2/GameView.swift
@@ -8,21 +8,43 @@
 import SwiftUI
 
 struct GameView: View {
+    @State private var offsetY: CGFloat = 0.0
     @Environment(\.dismiss) var dismiss
     @State var questionsAnswered = 0
     @State var currentLevel = 0
     @State var levelProgress = 0
     
     let question = ["what is the power house of the cell"]
-    let possibleAnswers = ["mitocondria"]
+    let wrongAnswers = ["cellwall"]
+    let correctAnswers = ["mitocondria"]
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    let playersAnswers = [""]
+    var playersAnswers = [""]
+    
+    func gradeAnswer(){
+        
+    }
+    
     
     var body: some View {
-        Image("sky background loop").navigationBarBackButtonHidden()
-            .onReceive(timer) {_ in 
-                print("hello")
+        GeometryReader { geometry in
+            ZStack{
+                Image("sky background loop").navigationBarBackButtonHidden()
+                    .scaledToFill()
+                    .offset(y: offsetY)
+                    .clipped()
+                Image("sky background loop")
+                    .scaledToFill()
+                    .offset(y: offsetY)
+                    .clipped()
             }
+            .onReceive(timer) { _ in
+                            offsetY += 2
+                            if offsetY >= geometry.size.height {
+                                offsetY = 0
+                            }
+        }
+        .edgesIgnoringSafeArea(.all)
+
     }
     
 }
