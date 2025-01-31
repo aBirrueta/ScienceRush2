05a32946e58fa4463f495c83843c0d32eb4b0092
aBirrueta <alejandrobirru3ta@gmail.com>
2025-01-31 10:08:38 -0800

    fixed error, and made the answers move indepenently
    before i had the answers move with the sky but now they move alone which fixed a problem i was a having.

--- ScienceRush2/GameView.swift
+++ ScienceRush2/GameView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct GameView: View {
     
-    @State private var offsetY: CGFloat = 0.0
+    @State private var skyOffSetY: CGFloat = 0.0
+    @State private var answersOffSetY: CGFloat = 0.0
     @Environment(\.dismiss) var dismiss
     @State var questionsAnswered = 0
     @State var currentLevel = 0
@@ -18,7 +19,9 @@ struct GameView: View {
     @State var questionShowing = 0.0
     @State var trueShowing = 0.0
     @State var falseShowing = 0.0
-    let question = ["What is the basic unit of life?",
+    let question =
+                [
+                    ["What is the basic unit of life?",
                     "What part of the cell controls its activities?",
                     "Which structure provides energy to the cell?",
                     "What surrounds and protects animal cells?",
@@ -33,21 +36,30 @@ struct GameView: View {
                     "What is the main purpose of the cytoskeleton in a cell?",
                     "What are the two main types of cells?",
                     "What is the function of the cell membrane?"]
-    let answers = [["Atom", "Cell", "Tissue", "Organ"],
-                   ["Mitochondria", "Nucleus", "Cell membrane", "Ribosome"],
-                   ["Lysosome", "Ribosome", "Mitochondria", "Chloroplast"],
-                   ["Cell wall", "Cytoplasm", "Cell membrane", "Nucleus"],
-                   ["Chloroplast", "Vacuole", "Ribosome", "Mitochondria"],
-                   ["Cytoplasm", "Nucleus", "Cell membrane", "Golgi apparatus"],
-                   ["Mitochondria", "Ribosome", "Endoplasmic reticulum", "Lysosome"],
-                   ["Golgi apparatus", "Endoplasmic reticulum", "Ribosome", "Lysosome"],
-                   ["Nucleus", "Mitochondria", "Cell wall", "Cell membrane"],
-                   ["To store energy", "To store water and nutrients", "To produce proteins", "To protect the cell"],
-                   ["To produce energy", "To break down waste materials", "To transport molecules", "To store DNA"],
-                   ["Smooth Endoplasmic Reticulum", "Ribosome", "Mitochondria", "Golgi apparatus"],
-                   ["To produce proteins", "To provide structure and support", "To store genetic material", "To break down toxins"],
-                   ["Plant and Animal", "Prokaryotic and Eukaryotic", "Bacteria and Virus", "Nucleus and Non-Nucleus"],
-                   ["To provide energy", "To control what enters and exits the cell", "To produce proteins", "To store waste"]]
+                ]
+    let answers =
+            [
+                [
+                    ["Atom", "Cell", "Tissue", "Organ"],
+                    ["Mitochondria", "Nucleus", "Cell membrane", "Ribosome"],
+                    ["Lysosome", "Ribosome", "Mitochondria", "Chloroplast"],
+                    ["Cell wall", "Cytoplasm", "Cell membrane", "Nucleus"],
+                    ["Chloroplast", "Vacuole", "Ribosome", "Mitochondria"],
+                    ["Cytoplasm", "Nucleus", "Cell membrane", "Golgi apparatus"],
+                    ["Mitochondria", "Ribosome", "Endoplasmic reticulum", "Lysosome"],
+                    ["Golgi apparatus", "Endoplasmic reticulum", "Ribosome", "Lysosome"],
+                    ["Nucleus", "Mitochondria", "Cell wall", "Cell membrane"],
+                    ["To store energy", "To store water and nutrients", "To produce proteins", "To protect the cell"],
+                    ["To produce energy", "To break down waste materials", "To transport molecules", "To store DNA"],
+                    ["Smooth Endoplasmic Reticulum", "Ribosome", "Mitochondria", "Golgi apparatus"],
+                    ["To produce proteins", "To provide structure and support", "To store genetic material", "To break down toxins"],
+                    ["Plant and Animal", "Prokaryotic and Eukaryotic", "Bacteria and Virus", "Nucleus and Non-Nucleus"],
+                    ["To provide energy", "To control what enters and exits the cell", "To produce proteins", "To store waste"]
+                ]
+            ]
+                
+                
+                 
     let correctAnswers = ["Cell","Nucleus","Mitochondria","Cell membrane","Chloroplast","Cytoplasm","Ribosome","Golgi apparatus","Cell wall","To store water and nutrients","To break down waste materials","Smooth Endoplasmic Reticulum","To provide structure and support","Prokaryotic and Eukaryotic","To control what enters and exits the cell"]
     var playersAnswers = [""]
     
@@ -58,37 +70,39 @@ struct GameView: View {
     
     func gradeAnswer(playerAnswer: String) { //function determines if question right or wrong
         if playerAnswer == correctAnswers[currentQuestion] {
-            twoSec = 200
+            twoSec = 100
             trueShowing = 1
             questionShowing = 0.0
         }
         else {
-            twoSec = 200
+            twoSec = 100
             
             falseShowing = 1
             questionShowing = 0.0
         }
+        if question[currentLevel].count - 1 != currentQuestion {
+            currentQuestion += 1
+            print(question[currentLevel].count)
+            print(currentQuestion)
+        }
+        answersOffSetY = -200
     } //closing gradeAnswer
-    func questionAnswered(playerAnswer: String) {
-        currentQuestion += 1
         
-    }
-    
     var body: some View {
         GeometryReader { geometry in
             ZStack{
                 Image("sky background loop")
                     .resizable()
-                    .offset(y: offsetY)
+                    .offset(y: skyOffSetY)
                     .clipped()
                     .aspectRatio(contentMode: .fill)
                 Image("sky background loop")
                     .resizable()
-                    .offset(y: offsetY-geometry.size.height)
+                    .offset(y: skyOffSetY-geometry.size.height)
                     .clipped()
                     .aspectRatio(contentMode: .fill)
                 
-                Text(question[currentQuestion]+"?")
+                Text(question[currentLevel][currentQuestion]+"?")
                     .position(x: 200, y: 100)
                     .fontWeight(.black)
                     .opacity(questionShowing)
@@ -109,30 +123,29 @@ struct GameView: View {
                         .opacity(trueShowing)
                         
                     HStack{
-                        Button(answers[currentQuestion][0]) {
-                            gradeAnswer(playerAnswer: answers[currentQuestion][0])
-                            
-                            currentQuestion += 1                        }
+                        Button(answers[currentLevel][currentQuestion][0]) {
+                            gradeAnswer(playerAnswer: answers[currentLevel][currentQuestion][0])
+                        }
                         .buttonBorderShape(.capsule)
                         .buttonStyle(.borderedProminent)
-                        Button(answers[currentQuestion][1]) {
-                            gradeAnswer(playerAnswer: answers[currentQuestion][1])
-                            currentQuestion += 1                          }
+                        Button(answers[currentLevel][currentQuestion][1]) {
+                            gradeAnswer(playerAnswer: answers[currentLevel][currentQuestion][1])
+                         }
                         .buttonBorderShape(.capsule)
                         .buttonStyle(.borderedProminent)
-                        Button(answers[currentQuestion][2]) {
-                            gradeAnswer(playerAnswer: answers[currentQuestion][2])
-                            currentQuestion += 1                          }
+                        Button(answers[currentLevel][currentQuestion][2]) {
+                            gradeAnswer(playerAnswer: answers[currentLevel][currentQuestion][2])
+                        }
                         .buttonBorderShape(.capsule)
                         .buttonStyle(.borderedProminent)
-                        Button(answers[currentQuestion][3]) {
-                            gradeAnswer(playerAnswer: answers[currentQuestion][3])
-                            currentQuestion += 1                          }
+                        Button(answers[currentLevel][currentQuestion][3]) {
+                            gradeAnswer(playerAnswer: answers[currentLevel][currentQuestion][3])
+                        }
                         .buttonBorderShape(.capsule)
                         .buttonStyle(.borderedProminent)
                     }
                     .opacity(questionShowing)
-                    .offset(y: offsetY-geometry.size.height)
+                    .offset(y: answersOffSetY-geometry.size.height)
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 
@@ -142,6 +155,7 @@ struct GameView: View {
             .navigationBarBackButtonHidden()
         
             .onReceive(timer) { _ in
+                //counters action
                 if counter == 0 {
                     questionShowing = 1.0
                 }
@@ -150,11 +164,20 @@ struct GameView: View {
                     trueShowing = 0
                     questionShowing = 1.0
                 }
-                offsetY += 2
-                if offsetY >= geometry.size.height {
-                    offsetY = 0
-                    currentQuestion += 1
+                //scrolling for sky
+                skyOffSetY += 2
+                if skyOffSetY >= 874.0 {
+                    skyOffSetY = 0
                 }
+                //scrolling for answers
+                answersOffSetY += 2
+                if answersOffSetY >= 1200 {
+                    answersOffSetY = 0
+                    gradeAnswer(playerAnswer: "n/a")
+                }
+                    
+                
+                //counters
                 if twoSec == 1 {
                     if counter != 0 {
                         counter -= 1
