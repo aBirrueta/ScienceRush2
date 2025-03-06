d97e2595246c6f6514aa24ebda413a44bec4832a
aBirrueta <alejandrobirru3ta@gmail.com>
2025-03-06 10:10:02 -0800

    made buttons to move player instead of dragging still working on the movments

--- ScienceRush2/PlayerView.swift
+++ ScienceRush2/PlayerView.swift
@@ -8,20 +8,26 @@
 import SwiftUI
 
 struct PlayerView: View {
-    @State private var offset = CGSize.zero
+    @State private var offset: Double = 100.5
+    let screenWidth: Double = Double(UIScreen.main.bounds.size.width)
+
     var body: some View {
+        //offset = screenWidth/4.0
         Image("rocket")
             .resizable()
                .frame(width: 100, height: 100)
                .aspectRatio(contentMode: .fit)
-               .offset(offset)
-               .gesture(
-                   DragGesture()
-                       .onChanged { gesture in
-                           offset = gesture.translation
-                       }
-                   )
+               .offset(x:offset)
+        HStack{
+            Button("left") {
+                offset -= screenWidth/4.0
+            }
+            Button("right") {
+                offset += screenWidth/4.0
+            }
+        }
     }
+    
 }
 
 #Preview {
